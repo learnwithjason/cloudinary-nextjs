bde75152ce7ebb9a0b3035bc80abcfeb9c657e1a
Jason Lengstorf <jason@lengstorf.com>
2022-11-17 11:09:37 -0800

    feat: code after episode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import { CldImage, CldOgImage } from 'next-cloudinary';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -9,12 +9,84 @@ export default function Home() {
         <title>Next.js with Cloudinary!</title>
       </Head>
 
+      <CldOgImage
+        src="jason.af/jason-bg.jpg"
+        alt="Jason Lengstorf"
+        removeBackground
+        underlay="jason.af/jason-and-marisa-barcelona"
+        overlays={[
+          {
+            text: {
+              color: 'white',
+              fontFamily: 'Impact',
+              fontSize: 250,
+              text: 'BUTTS',
+              border: '20px_solid_black',
+              stroke: true,
+            },
+            position: {
+              gravity: 'north',
+              y: 80,
+            },
+          },
+          {
+            text: {
+              color: 'white',
+              fontFamily: 'Impact',
+              fontSize: 250,
+              text: 'ARE THE BEST',
+              border: '20px_solid_black',
+              stroke: true,
+            },
+            position: {
+              gravity: 'south',
+              y: 80,
+            },
+          },
+        ]}
+      />
+
       <main className={styles.main}>
         <h1 className={styles.title}>Next.js with Cloudinary!</h1>
 
-        <img
-          src="https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,w_500,h_500,c_thumb,g_faces,z_0.75/press/jason-lengstorf-ac-alley3.jpg"
+        <CldImage
+          src="jason.af/jason-bg.jpg"
           alt="Jason Lengstorf"
+          sizes="100vw"
+          height={4267}
+          width={6400}
+          removeBackground
+          underlay="jason.af/jason-and-marisa-barcelona"
+          overlays={[
+            {
+              text: {
+                color: 'white',
+                fontFamily: 'Impact',
+                fontSize: 250,
+                text: 'BUTTS',
+                border: '20px_solid_black',
+                stroke: true,
+              },
+              position: {
+                gravity: 'north',
+                y: 80,
+              },
+            },
+            {
+              text: {
+                color: 'white',
+                fontFamily: 'Impact',
+                fontSize: 250,
+                text: 'ARE THE BEST',
+                border: '20px_solid_black',
+                stroke: true,
+              },
+              position: {
+                gravity: 'south',
+                y: 80,
+              },
+            },
+          ]}
         />
       </main>
     </div>
